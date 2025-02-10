b55e3d25900a3d2dc4eeaba291e219eb8ad292d3
Zidan <zidantaufiqurahman@gmail.com>
2025-02-10 11:09:09 +0700

    halo

--- hello world.c
+++ hello world.c
@@ -4,8 +4,10 @@ int main() {
 	int i=0;
 	printf("Hello world!\n");
 	printf("Assalamualaikum\n");
+	printf("halo\n");
 	printf("Waalaikumsalam\n");
 	while(i<100){
 		printf("Assalamualaikum\n");
 	}
+
 }
